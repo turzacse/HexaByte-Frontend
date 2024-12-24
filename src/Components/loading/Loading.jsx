af746e505ed18e0ff64b32b5aea1810760ced194
unknown <khayatiadel10@gmail.com>
2024-12-24 17:45:27 +0100

    add loading

---
+++ src/Components/loading/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './loading.css'
+const Loading = () => {
+  return (
+    <div className='loading-container'>
+    <span class="loader"></span>
+  </div>
+  )
+}
+
+export default Loading
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,31 +1,45 @@
-import React, { useEffect } from 'react';
-import logo from "../../assets/Image/logo.png"
-import Section from '../../Components/Section/Section';
-import HeroSection from './Hero/HeroSection';
-import AboutUs from './AboutUS/AboutUs';
-import Services from './Service/Service';
-import CaseStudies from './CaseStudies/CaseStudies';
-import Testimonials from './Testimonials/Testimonials';
-import MeetOurTeam from './MeetOurTeam/MeetOurTeam';
-import GetInTouch from './GetInTouch/GetInTouch';
-import Banner from './Banner/Banner';
+import React, { useEffect, useState } from "react";
+import logo from "../../assets/Image/logo.png";
+import Section from "../../Components/Section/Section";
+import HeroSection from "./Hero/HeroSection";
+import AboutUs from "./AboutUS/AboutUs";
+import Services from "./Service/Service";
+import CaseStudies from "./CaseStudies/CaseStudies";
+import Testimonials from "./Testimonials/Testimonials";
+import MeetOurTeam from "./MeetOurTeam/MeetOurTeam";
+import GetInTouch from "./GetInTouch/GetInTouch";
+import Banner from "./Banner/Banner";
+import Loading from "../../Components/loading/Loading";
 
 const Home = () => {
-    useEffect(() => {
-        window.scroll(0,0);
-    } ,[])
-    return (
-        <div className=''>
-            {/* <HeroSection/> */}
-            <Banner/>
-            <AboutUs/>
-            <Services/>
-            <CaseStudies/>
-            <Testimonials/>
-            <MeetOurTeam/>
-            <GetInTouch/>
-        </div>
-    );
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <div className="">
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          {/* <HeroSection/> */}
+          <Banner />
+          <AboutUs />
+          <Services />
+          <CaseStudies />
+          <Testimonials />
+          <MeetOurTeam />
+          <GetInTouch />
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Home;
